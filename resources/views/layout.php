2ae922b2779178d15c7c8eb820bf1848c3171851
Vinayak <vinayak.anivase@gmail.com>
2026-05-26 23:01:44 +0530

    Use yield() in layout template (section() with a value is now a setter)

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $this->e($this->section('title', 'App')) ?></title>
+    <title><?= $this->e($this->yield('title', 'App')) ?></title>
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
@@ -13,7 +13,7 @@
 </nav>
 
 <main>
-    <?= $this->section('content') ?>
+    <?= $this->yield('content') ?>
 </main>
 
 </body>
